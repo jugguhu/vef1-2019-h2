f4342ad64d47aae7a347c9bbd458a27acfb02069
Sindri Magnússon <sim42@hi.is>
2019-11-28 12:05:30 +0000

    helper

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import List from './lib/list';
+import loadLecture from './lib/lecture';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
 
   if (isLecturePage) {
-
+    loadLecture();
   } else {
     const list = new List();
     list.load();
