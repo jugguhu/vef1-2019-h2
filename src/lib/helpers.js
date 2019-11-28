d323f56bf4265cc3b041864616f2566a145621b9
Jón Gunnar Hannesson <jgh060@gmail.com>
2019-11-28 22:01:47 +0000

    elements a forsidu

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -22,3 +22,18 @@ export function get_slug() {
   }
   return 0;
 }
+
+export function el(name, ...children) {
+  const element = document.createElement(name);
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
+}
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,4 +1,4 @@
-import { empty, get_json } from './helpers';
+import { empty, get_json, el } from './helpers';
 
 export default class List {
   constructor() {
@@ -8,20 +8,36 @@ export default class List {
 
   load() {
     // empty(this.container);
-    this.getLectures();
+    this.getLectures()
+      .then(data => this.showLectures(data.lectures));
   }
 
   getLectures() {
-    fetch(this.lectures)
+    return fetch(this.lectures)
       .then((res) => {
         if (!res.ok) {
           throw new Error('Non 200 status');
         }
         return res.json();
-      })
-      .then((data) => {
-        console.log(data.lectures);
       });
+  }
 
+  showLectures(data) {
+    for (let i of data) { //eslint-disable-line
+      const element = el('div',
+        el('a',
+          el('img'),
+          el('div',
+            el('span', i.category),
+            el('h1', i.title))));
+      element.classList.add('fyrirlestrar__col');
+      element.querySelector('div').classList.add('fyrirlestrar__col__content');
+      element.querySelector('span').classList.add('fyrirlestrar__col__content__category');
+      element.querySelector('h1').classList.add('fyrirlestrar__col__content__title');
+      if (i.thumbnail) {
+        element.querySelector('img').setAttribute('src', i.thumbnail);
+      }
+      document.querySelector('.fyrirlestrar').appendChild(element);
+    }
   }
 }
